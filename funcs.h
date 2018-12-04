ea69897d3de590964a3497a4c75dcb8cd888c1af
Komarova <komarovaleriya18@gmail.com>
2018-12-04 14:32:24 +0300

    add function MyDiv()

--- funcs.h
+++ funcs.h
@@ -2,5 +2,5 @@
 // заголовочный файл funcs.h
 #ifndef __FUNCS_H__ #define __FUNCS_H__
 double MySum(double, double); double MySub(double, double);
-double MyMul(double, double);
+double MyMul(double, double); double MyDiv(double a, double b)
 #endif // __FUNCS_H__
